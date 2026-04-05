a4042f6de2371268ca88a421ead3865b389d3193
Huayu Liang <huayu98@vt.edu>
2026-04-04 23:34:31 -0400

    Update home.test.tsx and blog.test.tsx

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -2,20 +2,20 @@ import { render, screen } from '@testing-library/react'
 import HomePage from '../pages/index'
 
 describe('Home', () => {
-    it('should have the text"Contact"', () => {
-        render(<HomePage />) //ARRANCE
+    it('should have the text "Contact"', () => {
+        render(<HomePage />) //ARRANGE
         const myElem  = screen.getByText('Contact') //ACT
         expect(myElem).toBeInTheDocument() //ASSERT
     })
 
     it('should have the text "Resume"', () => {
-        render(<HomePage />) //ARRANCE
+        render(<HomePage />) //ARRANGE
         const myElem  = screen.getByText('Resume') //ACT
         expect(myElem).toBeInTheDocument() //ASSERT
     })
 
     it('should have at least one img element', () => {
-        render(<HomePage />) //ARRANCE
+        render(<HomePage />) //ARRANGE
         const imgs  = screen.getAllByRole('img') //ACT
         expect(imgs.length).toBeGreaterThan(0) //ASSERT
     })
